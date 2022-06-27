5fe5fb61172bbfe094336958ae641e9bacb292aa
antedo <edoardesd@gmail.com>
2022-06-27 09:32:15 +0200

    topic change

--- listerner.js
+++ listerner.js
@@ -1,13 +1,11 @@
-
-// Imports
 const fs = require('fs');
 const mqtt = require('mqtt');
-const Jimp = require("jimp");
 
 // Global variables
-const LOCAL_DIR_TO_COPY_TO = "/home/ubuntu/raw_field_data";
-const TOPIC_TO_SUBSCRIBE_TO = "smart/parking/polimi";
+const LOCAL_DIR_TO_COPY_TO = "./images/";
+const TOPIC_TO_SUBSCRIBE_TO = "smart/parking/polimi/image";
 const HOST = "broker.hivemq.com"
+const SAVE_EVERY = 300000 //milliseconds
 
 const clientId = `parking_${Math.random().toString(16).slice(3)}`
 
@@ -17,6 +15,12 @@ const client = mqtt.connect('mqtt://'+HOST, {
     connectTimeout: 4000,
     reconnectPeriod: 1000,
 });
+
+let buffer;
+let timer;
+let filename;
+let oldFilename = "";
+
 
 
 client.on('connect', function() {
@@ -31,17 +35,41 @@ client.on("connect", function(){
 
 
 client.on('message', function(topic, payload) {
-    console.log('Message received on', topic);
-
     if (topic === TOPIC_TO_SUBSCRIBE_TO) {
-        console.log('message::' + TOPIC_TO_SUBSCRIBE_TO);
-        // Reads file in form buffer => <Buffer ff d8 ff db 00 43 00 ...
-        // const buffer = fs.readFileSync("path-to-image.jpg");
-        // Pipes an image with "new-path.jpg" as the name.
-        fs.writeFileSync("new-path.jpg", payload);
+        console.log('message::' + topic);
+
+        buffer = Buffer.from(payload.toString(), "base64");
+        filename = getFilename()
     }
 });
 
 client.on('error', (error) => {
     console.log(error)
 })
+
+saveImagePeriodic()
+
+function saveImagePeriodic() {
+    timer = setInterval(function() {
+        if (filename != oldFilename) {
+            fs.writeFileSync(LOCAL_DIR_TO_COPY_TO + filename, buffer);
+            console.log("Pic saved: ", filename);
+            oldFilename = filename
+        }
+        else {
+            console.log("warning::pic already saved. Skipping")
+        }
+    }, SAVE_EVERY);
+}
+
+function getFilename() {
+    let date_ob = new Date();
+    let day = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = ("0" + date_ob.getHours()).slice(-2);
+    let minutes = ("0" + date_ob.getMinutes()).slice(-2);
+    let seconds = ("0" + date_ob.getSeconds()).slice(-2);
+
+    return year+month+day+"-"+hours+minutes+seconds+".png"
+}
